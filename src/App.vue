b093e0238f637fc30eb7ff291954581b3c6d5e45
yangm0910 <yangm_1111@163.com>
2019-04-12 17:43:45 +0800

    first ci

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="todo list"/>
   </div>
 </template>
 
@@ -12,6 +11,11 @@ export default {
   name: 'app',
   components: {
     HelloWorld
+  },
+  computed:{
+    now: function(){
+      return Date.now();
+    }
   }
 }
 </script>
@@ -21,7 +25,6 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
